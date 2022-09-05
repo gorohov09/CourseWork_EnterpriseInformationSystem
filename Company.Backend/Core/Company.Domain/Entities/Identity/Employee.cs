0094430fc20c14b3cb4eb6cbbdfdc55d2219689c
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-05 19:52:10 +0300

    Определение сущностей - Работник и Роль

---
+++ Company.Backend/Core/Company.Domain/Entities/Identity/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Company.Domain.Entities.Identity
+{
+    public class Employee : IdentityUser
+    {
+        public const string Administrator = "Admin";
+
+        public const string DefaultAdminPassword = "AdPAss_123";
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public string Position { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public long Phone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
---
+++ Company.Backend/Core/Company.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Company.Domain.Entities.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrators = "Administrators";
+
+        public const string Employees = "Employees";
+
+        public string Description { get; set; }
+    }
+}
