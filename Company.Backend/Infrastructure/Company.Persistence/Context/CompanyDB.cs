28d55c8378260a0c9c45d036049396782cca2258
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-05 19:54:04 +0300

    Создание контекста БД

---
+++ Company.Backend/Infrastructure/Company.Persistence/Context/CompanyDB.cs
@@ -0,0 +1,15 @@
+﻿using Company.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Persistence.Context
+{
+    public class CompanyDB : IdentityDbContext<Employee, Role, string>
+    {
+        public CompanyDB(DbContextOptions<CompanyDB> options)
+            : base(options)
+        {
+
+        }
+    }
+}
