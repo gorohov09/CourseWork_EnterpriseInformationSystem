d3e827742c1b9368db4ffd1bffc47d34ee1863fa
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-05 20:12:43 +0300

    Первые миграции

--- Company.Backend/Presentation/Company.WebAPI/Program.cs
+++ Company.Backend/Presentation/Company.WebAPI/Program.cs
@@ -1,4 +1,6 @@
+using Company.Domain.Entities.Identity;
 using Company.Persistence.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,30 @@ var services = builder.Services;
 
 services.AddDbContext<CompanyDB>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+services.AddIdentity<Employee, Role>()
+    .AddEntityFrameworkStores<CompanyDB>()
+    .AddDefaultTokenProviders();
+
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 3;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(20);
+
+});
 
 services.AddControllers();
 services.AddEndpointsApiExplorer();
