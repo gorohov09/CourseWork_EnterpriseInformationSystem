abb728d1056ed9c6d083331f3b441be221f15c17
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2022-09-05 20:03:07 +0300

    Подключение базы данных

--- Company.Backend/Presentation/Company.WebAPI/Program.cs
+++ Company.Backend/Presentation/Company.WebAPI/Program.cs
@@ -1,15 +1,20 @@
+using Company.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddDbContext<CompanyDB>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
